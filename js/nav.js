b2b12d24a01ba6030130314159442497766afd04
arizals <ahmadrizalichal@ymail.com>
2020-07-28 13:57:54 +0700

    first review commit

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Activate sidebar nav
-    var elems = document.querySelectorAll(".sidenav");
+    const elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
     loadNav();
 
     function loadNav() {
-        var xhttp = new XMLHttpRequest();
+        const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status != 200) return;
+            if (this.readyState === 4) {
+                if (this.status !== 200) return;
 
                 // Muat daftar tautan menu
                 document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.querySelectorAll(".sidenav a, .topnav a").forEach(function (elm) {
                     elm.addEventListener("click", function (event) {
                         // Tutup sidenav
-                        var sidenav = document.querySelector(".sidenav");
+                        const sidenav = document.querySelector(".sidenav");
                         M.Sidenav.getInstance(sidenav).close();
 
                         // Muat konten halaman yang dipanggil
@@ -34,18 +34,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Load page content
-    var page = window.location.hash.substr(1);
+    let page = window.location.hash.substr(1);
     if (page == "") page = "home";
     loadPage(page);
 
     function loadPage(page) {
-        var xhttp = new XMLHttpRequest();
+        const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                var content = document.querySelector("#body-content");
-                if (this.status == 200) {
+            if (this.readyState === 4) {
+                const content = document.querySelector("#body-content");
+                if (this.status === 200) {
                     content.innerHTML = xhttp.responseText;
-                } else if (this.status == 404) {
+                } else if (this.status === 404) {
                     content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
                 } else {
                     content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
